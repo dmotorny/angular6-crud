e493570b012977105d7bee8f8d3790060cbcd785
DM <dmotorny@gmail.com>
2018-05-12 00:22:41 +0300

    migration from v.5 to v.6 Angular

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,10 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
-  enableProdMode();
+    enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+document.addEventListener('startLoad', () => {
+    platformBrowserDynamic().bootstrapModule(AppModule)
+        .catch(err => console.log(err));
+});
