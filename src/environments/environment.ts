930c0b1799356a89db0ae336db17a4a3abbcdd98
dmotorny <dmotorny@users.noreply.github.com>
2018-08-19 17:20:59 +0300

    Update environment.ts
    tabulation correction

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
-  production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyDQCSkT3pzlnIukOXnS54wPr_I3cpHOum4",
-    authDomain: "angular6-crud.firebaseapp.com",
-    databaseURL: "https://angular6-crud.firebaseio.com",
-    projectId: "angular6-crud",
-    storageBucket: "",
-    messagingSenderId: "476071676293"
-  }
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyDQCSkT3pzlnIukOXnS54wPr_I3cpHOum4",
+        authDomain: "angular6-crud.firebaseapp.com",
+        databaseURL: "https://angular6-crud.firebaseio.com",
+        projectId: "angular6-crud",
+        storageBucket: "",
+        messagingSenderId: "476071676293"
+    }
 };
